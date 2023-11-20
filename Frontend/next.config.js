3008961b07a94aa0bf1e3f74a7752e7eab2c5728
akanshakaranjkhele <53134476+akanshakaranjkhele@users.noreply.github.com>
2023-11-20 14:55:57 +0530

    add

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
 
 module.exports = {
   env: {
-    apiKey: "http://51.20.107.142:3000/",
-    productKey: "http://51.20.107.142:3000/products/name",
+    apiKey: "http://16.171.110.47:3000/",
+    productKey: "http://16.171.110.47:3000/products/name",
   },
 };
